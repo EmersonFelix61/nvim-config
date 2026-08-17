592c7d4bcebc79e91036c50de1501190ef1cb089
EmersonFelix61 <emerson.felix61@hotmail.com>
2026-08-17 10:25:17 +0100

    feat(mini.jump2d): improve jump labels

--- lua/plugins/themes/available.lua
+++ lua/plugins/themes/available.lua
@@ -1,5 +1,62 @@
 return {
-  { 'rebelot/kanagawa.nvim' },
+  {
+    'rebelot/kanagawa.nvim',
+    opts = {
+      -- Mantém a variante Wave; estas opções só entram em vigor ao selecioná-la.
+      theme = 'wave',
+      undercurl = true,
+      dimInactive = true,
+      terminalColors = true,
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              -- Integra a coluna de sinais ao fundo sem alterar a paleta Wave.
+              bg_gutter = 'none',
+            },
+          },
+        },
+      },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          -- Uma superfície consistente para hover, assinatura, completion e popups.
+          NormalFloat = { bg = theme.ui.bg_m3 },
+          FloatBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.bg_m3 },
+          FloatTitle = { fg = theme.ui.special, bg = theme.ui.bg_m3, bold = true },
+
+          -- Telescope preserva seu layout, mas passa a usar a mesma superfície
+          -- e seleção dos outros menus transitórios.
+          TelescopePromptNormal = { bg = theme.ui.bg_m3 },
+          TelescopePromptBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.bg_m3 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+          TelescopeResultsBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.bg_m3 },
+          TelescopePreviewNormal = { bg = theme.ui.bg_m3 },
+          TelescopePreviewBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.bg_m3 },
+          TelescopeTitle = { fg = theme.ui.special, bg = theme.ui.bg_m3, bold = true },
+          TelescopeSelection = { bg = theme.ui.bg_p2 },
+
+          -- Pmenu é compartilhado por nvim-cmp e pelo popupmenu do Noice.
+          Pmenu = { fg = theme.ui.pmenu.fg, bg = theme.ui.bg_m3 },
+          PmenuSel = { fg = theme.ui.pmenu.fg_sel, bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
+
+          -- Which-key, Neo-tree em modo flutuante e Noice herdam a mesma
+          -- linguagem visual sem reposicionar nenhuma janela.
+          WhichKeyNormal = { bg = theme.ui.bg_m3 },
+          WhichKeyBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.bg_m3 },
+          WhichKeyTitle = { fg = theme.ui.special, bg = theme.ui.bg_m3, bold = true },
+          NeoTreeFloatNormal = { bg = theme.ui.bg_m3 },
+          NeoTreeFloatBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.bg_m3 },
+          NeoTreeFloatTitle = { fg = theme.ui.special, bg = theme.ui.bg_m3, bold = true },
+          NeoTreeCursorLine = { bg = theme.ui.bg_p2 },
+          NoiceCmdlinePopupBorder = { fg = theme.ui.float.fg_border },
+          NoiceCmdlinePopupTitle = { fg = theme.ui.special, bold = true },
+        }
+      end,
+    },
+  },
   { 'Yazeed1s/oh-lucy.nvim' },
   { 'scottmckendry/cyberdream.nvim' },
   { 'cpea2506/one_monokai.nvim' },
--- lua/plugins/utils/mini.lua
+++ lua/plugins/utils/mini.lua
@@ -2,6 +2,8 @@ return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
 
   config = function()
+    local i18n = require 'config.i18n'
+
     -- Better Around/Inside textobjects
     --
     -- Examples:
@@ -36,6 +38,39 @@ return { -- Collection of various small independent plugins/modules
 
     -- Comments
     require('mini.comment').setup()
+
+    -- Estende f/F/t/T e ; sem trocar a gramática dos motions nativos:
+    -- alcança várias linhas, destaca alvos e repete o último salto.
+    require('mini.jump').setup()
+
+    -- Labels iterativos para saltos dentro da janela atual. Os atalhos ficam
+    -- sob <leader>j para manter <CR> e todos os motions clássicos intactos.
+    local jump2d = require 'mini.jump2d'
+    jump2d.setup {
+      labels = 'asdfghjklqwertyuiopzxcvbnm',
+      view = {
+        dim = false,
+        n_steps_ahead = 1,
+      },
+      allowed_windows = {
+        current = true,
+        not_current = false,
+      },
+      mappings = {
+        start_jumping = '',
+      },
+    }
+
+    local function map_jump(keys, opts, desc)
+      vim.keymap.set({ 'n', 'x', 'o' }, keys, function()
+        jump2d.start(opts)
+      end, { desc = desc })
+    end
+
+    map_jump('<leader>j', jump2d.builtin_opts.default, i18n.t 'map.jump_labels')
+    map_jump('<leader>jw', jump2d.builtin_opts.word_start, i18n.t 'map.jump_words')
+    map_jump('<leader>jc', jump2d.builtin_opts.single_character, i18n.t 'map.jump_character')
+    map_jump('<leader>jl', jump2d.builtin_opts.line_start, i18n.t 'map.jump_line')
 
     -- Statusline
     local statusline = require 'mini.statusline'
