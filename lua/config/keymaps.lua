cf428f666c5517e4e1bc01f9f5c3f0f10f4d3a18
Emerson Da Silva Felix <emda-sil@c1r10s2.42porto.com>
2026-08-17 14:00:41 +0100

    fix unresolved merge markers in keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -82,7 +82,6 @@ local disabled_keys = {
   '<PageDown>',
 }
 
-<<<<<<< HEAD
 for _, key in ipairs(disabled_keys) do
   vim.keymap.set({ 'n', 'i', 'v', 'x' }, key, function()
     vim.notify('USA VIM MOTIONS, ANIMAL', vim.log.levels.WARN)
@@ -91,13 +90,3 @@ for _, key in ipairs(disabled_keys) do
     desc = 'Vim motions training',
   })
 end
-=======
--- for _, key in ipairs(disabled_keys) do
---   vim.keymap.set({ 'n', 'i', 'v', 'x' }, key, function()
---     vim.notify('USA VIM MOTIONS, ANIMAL', vim.log.levels.WARN)
---   end, {
---     silent = true,
---     desc = 'Vim motions training',
---   })
--- end
->>>>>>> f900f3c (update toggleterm and flake8)
